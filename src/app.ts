d27fa620abbe49dfb29a964b4ebb4992ee03221d
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-04 10:31:02 +0545

    feat: add server

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import { notFoundHandler } from "./middlewares/not-found-handler";
+import { errorHandler } from "./middlewares/error-handler";
+import healthRoutes from "./routes/health.routes";
+import env from "./configs/env";
+
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+import { setupSocket } from "./configs/socket";
+
+const app: Express = express();
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: env.CORS_ORIGIN
+  }
+});
+
+setupSocket(io);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true
+  })
+);
+app.use(helmet());
+app.use(cookieParser());
+app.use(morgan(env.NODE_ENV === "development" ? "dev" : "combined"));
+
+//? Routes
+app.get("/", (req: Request, res: Response) => {
+  res.redirect("/api/health");
+});
+
+app.use("/api/health", healthRoutes);
+
+// Not found handler (should be after routes)
+app.use(notFoundHandler);
+
+// Global error handler (should be last)
+app.use(errorHandler);
+
+export { io };
+
+export default server;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+import env from "./configs/env";
+import { logger } from "./utils/logger";
+import { configureGracefulShutdown } from "./utils/shutdown";
+
+const port = env.PORT || 9000;
+
+const server = app.listen(port, () => {
+  logger.info(`[server]: Server is running at http://localhost:${port}`);
+  logger.info(`[server]: Environment: ${env.NODE_ENV}`);
+});
+
+configureGracefulShutdown(server);
