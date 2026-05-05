6357c4dbecbdbbdb311a42b51b3b4076273b83af
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-05 19:38:09 +0545

    feat: implement socket connection handling

---
+++ src/configs/socket.ts
@@ -0,0 +1,11 @@
+import { Server } from "socket.io";
+
+export function setupSocket(io: Server) {
+  io.on("connection", socket => {
+    console.log("a user connected: ", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("a user disconnected: ", socket.id);
+    });
+  });
+}
